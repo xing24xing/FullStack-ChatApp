b82ad5a00ce73831950a2ec4816ac92c498aab55
xing24xing <khushipandey260304@gmail.com>
2024-10-08 21:57:04 +0530

    deployed 1.0

--- frontend/src/hooks/useGetOtherUser.js
+++ frontend/src/hooks/useGetOtherUser.js
@@ -7,20 +7,29 @@ import { BASE_URL } from '../utils/constant.js';
 const useGetOtherUser = () => {
   const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchOtherUsers = async () => {
-            try {
-                axios.defaults.withCredentials = true;
-                const res = await axios.get(`${BASE_URL}/api/v1/user`);
-                // store
-                console.log("other users -> ",res);
-                dispatch(setOtherUsers(res.data));
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchOtherUsers();
-    }, [])
+  useEffect(() => {
+    const fetchOtherUsers = async () => {
+      try {
+        // Set withCredentials for all axios requests
+        axios.defaults.withCredentials = true;
+
+        // Make the GET request to fetch users
+        const res = await axios.get(`${BASE_URL}/api/v1/user`);
+
+        // Log the response for debugging
+        console.log("other users -> ", res.data);
+
+        // Dispatch the action to store the users in the Redux state
+        dispatch(setOtherUsers(res.data));
+      } catch (error) {
+        // Log the error message
+        console.error("Error fetching other users:", error);
+      }
+    };
+
+    // Call the function to fetch users
+    fetchOtherUsers();
+  }, [dispatch]); // Add dispatch to the dependency array
 };
 
 export default useGetOtherUser;
